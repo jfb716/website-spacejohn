f994ab2961a1db60cde1efa9444399e04ee49291
John Black <john.f.black@icloud.com>
2016-03-05 12:26:52 -0500

    updating js non-secure pw to one file

---
+++ resources/js/pw.js
@@ -0,0 +1,91 @@
+function customPlayer() {
+          
+ 
+
+var pass = prompt("Password:");
+
+
+if (pass == "customplayer")
+{
+
+window.location.assign("http://spacejohn.com/customplayer.html")
+
+}
+
+else
+{
+
+alert("Sorry, wrong password");
+
+
+}
+};
+
+function lrPlayer() {
+          
+ 
+
+var pass = prompt("Password:");
+
+
+if (pass == "lrplayer")
+{
+
+window.location.assign("http://spacejohn.com/lrplayer.html")
+
+}
+
+else
+{
+
+alert("Sorry, wrong password");
+
+
+}
+};
+
+function jwPlayer() {
+          
+ 
+
+var pass = prompt("Password:");
+
+
+if (pass == "jwplayer")
+{
+
+window.location.assign("http://spacejohn.com/jwplayer.html")
+
+}
+
+else
+{
+
+alert("Sorry, wrong password");
+
+
+}
+};
+
+function flowPlayer() {
+          
+ 
+
+var pass = prompt("Password:");
+
+
+if (pass == "flowplayer")
+{
+
+window.location.assign("http://spacejohn.com/flowplayer.html")
+
+}
+
+else
+{
+
+alert("Sorry, wrong password");
+
+
+}
+};
