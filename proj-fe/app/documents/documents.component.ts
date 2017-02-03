6649bff7bc9bb40b5121afd38ad17c437bb41bed
John Black <john.f.black@icloud.com>
2017-02-02 20:59:51 -0500

    styled documents page and nav bar

--- proj-fe/app/documents/documents.component.ts
+++ proj-fe/app/documents/documents.component.ts
@@ -4,7 +4,8 @@ import { Document } from './document';
 @Component({
   moduleId: module.id,
   selector: 'documents',
-  templateUrl: 'documents.component.html'
+  templateUrl: 'documents.component.html',
+  styleUrls: ['documents.component.css']
 })
 
 export class DocumentsComponent {
@@ -15,21 +16,21 @@ export class DocumentsComponent {
       description: 'Placeholder Description',
       file_url: 'http://google.com',
       updated_at: '01/29/2017',
-      image_url: 'http://google.com',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg/1280px-Mistakes-to-avoid-when-hiring-freelancers.jpg',
     },
     {
       title: 'My Second Doc',
       description: 'Placeholder Description',
       file_url: 'http://google.com',
       updated_at: '01/29/2017',
-      image_url: 'http://google.com',
+      image_url: 'https://cdn.pixabay.com/photo/2015/01/21/14/14/freelancer-606762_960_720.jpg',
     },
     {
       title: 'My Third Doc',
       description: 'Placeholder Description',
       file_url: 'http://google.com',
       updated_at: '01/29/2017',
-      image_url: 'http://google.com',
+      image_url: 'https://cdn.pixabay.com/photo/2014/12/22/19/59/macbook-577758_960_720.jpg',
     }
   ]
 }
