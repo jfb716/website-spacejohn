daf72e120c88d805a3547d6680a38a05f7a12929
John Black <john.f.black@icloud.com>
2017-03-28 21:04:32 -0400

    updates

--- proj-documents/db/seeds.rb
+++
@@ -1,4 +0,0 @@
-
-10.times do |d|
-ProjDocument.create!(title: "Document #{d}", description: "description holder", file_url: "https://www.smashingmagazine.com/wp-content/uploads/2016/01/07-responsive-image-example-castle-7-opt.jpg", image_url: "https://www.smashingmagazine.com/wp-content/uploads/2016/01/07-responsive-image-example-castle-7-opt.jpg")
-end
